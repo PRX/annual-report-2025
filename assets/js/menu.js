c6d51d06164ea509c4bc975894c26b2f7be32e29
Brandon Hundt <brandonhundt@gmail.com>
2025-09-25 15:00:10 -0500

    fix(nav): mobile layout and styles

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,18 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggleBtn = document.getElementById('nav-toggle');
   const body = document.body;
+  const main = document.getElementById('layout-content');
 
   // set initial state
   const setInitialState = () => {
-    if (window.innerWidth >= 768) {
-      body.setAttribute('data-menu-open', 'true'); // desktop default open
+    const isMenuOpen = body.dataset.menuOpen === 'true';
+    if (isMenuOpen && window.innerWidth < 768) {
+      main.setAttribute('inert', ''); // desktop default open
     } else {
-      body.setAttribute('data-menu-open', 'false'); // mobile default closed
+      main.removeAttribute('inert'); // mobile default closed
     }
   };
 
+  function handleResize(){
+    setInitialState();
+  };
+
   setInitialState();
-  window.addEventListener('resize', setInitialState);
+  window.addEventListener('resize', handleResize);
 
   // toggle handler
   toggleBtn.addEventListener('click', () => {
